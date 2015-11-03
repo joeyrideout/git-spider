9ee3611a6799f742f7faa55ce79b68e1872046c3
Joey Rideout <joeyrrideout@gmail.com>
2015-11-02 23:22:23 -0800

    Made spider smarter.
    * http and https are now checked
    
    * spider.sh is more maintainable
    
    * results are now printed to stdout
    
    * results are now colour-coded
    
    * script now ignores robots.txt
    
    * script now has a shorter (5s) timeout with no retries

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # git-spider
 Simple bash script I wrote to crawl a list of host names for exposed .git repositories.
 
-Will take in a list of hostnames, targets.txt, and write wget results to spider.out.
+Will take in a list of hostnames, targets.txt, and write wget results to stdout.
 
 Example usage:
 ```
@@ -9,5 +9,4 @@ echo "mysite.example.com" > targets.txt
 chmod +x spider.sh
 ./spider.sh
 
-cat spider.out | grep "Remote file exists"
 ```
--- spider.sh
+++ spider.sh
@@ -1,3 +1,24 @@
+file=.git/HEAD
+logfile="spider.log"
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+wgetargs="-e robots=off --no-check-certificate --spider --tries=1 --timeout=5 --max-redirect 0"
 while read p; do
-  wget https://$p/.git/HEAD --no-check-certificate --spider --tries=2 --timeout=10 > spider.out 2> spider.out
+  url=http://$p/$file
+  wget $url $wgetargs >> $logfile 2>> $logfile
+  if [[ $? != 0 ]]
+  then
+    echo "${green}$url not found${reset}"
+    url=https://$p/$file
+    wget $url $wgetargs >> $logfile 2>> $logfile
+    if [[ $? != 0 ]]
+    then
+      echo "${green}$url not found${reset}"
+    else
+      echo "${red}$url found${reset}"
+    fi
+  else
+    echo "${red}$url found${reset}"
+  fi
 done < targets.txt
